18325c101477bf88eef6f067620c790456c74d99
aleyn <aleyn.coello@gmail.com>
2025-03-02 11:24:27 +0000

    InMemoryUserDetails

--- src/main/java/com/aleyn/app_security/security/SecurityConfig.java
+++ src/main/java/com/aleyn/app_security/security/SecurityConfig.java
@@ -4,28 +4,47 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
 
-    // In spring 6 it is not necessary to put @Autowired for HttpSecurity,
-    // because spring already injects it automatically
-
-    // This is the default configuration of spring security
     @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity http) {
-        try {
-            http.authorizeHttpRequests(auth ->
-                    auth.requestMatchers("/loans", "/balance", "/account", "/cards")
-                            .authenticated()
-                            .anyRequest().permitAll())
-                    .formLogin(Customizer.withDefaults())
-                    .httpBasic(Customizer.withDefaults());
-            return http.build();
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(auth ->
+                        auth.requestMatchers("/loans", "/balance", "/account", "/cards")
+                                .authenticated()
+                                .anyRequest().permitAll())
+                .formLogin(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
+        UserDetails admin = User
+                .withUsername("admin")
+                .password("to_be_encoded")
+                .authorities("ADMIN")
+                .build();
+
+        UserDetails user = User
+                .withUsername("user")
+                .password("to_be_encoded")
+                .authorities("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(admin, user);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
     }
 
 }
